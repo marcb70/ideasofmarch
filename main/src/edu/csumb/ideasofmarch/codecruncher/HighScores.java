142808a9a61a59944edd72c37daf59f75e0b85d6
leighannewarner <leighannewarner@gmail.com>
2013-03-16 13:58:14 -0700

    Global high scores fills with dummy values. Attempted to write get and put from the server, will test after backend is finished.

--- main/src/edu/csumb/ideasofmarch/codecruncher/HighScores.java
+++ main/src/edu/csumb/ideasofmarch/codecruncher/HighScores.java
@@ -1,19 +1,38 @@
 package edu.csumb.ideasofmarch.codecruncher;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class HighScores extends Activity {
 
 	private static final int NUM_GLOBAL_SCORES_TO_DISPLAY = 10;
-	
+
 	private SharedPreferences localHighScores;
 	private TextView localScoresTextView[];
 	private TextView localTotalTextView;
 	private int localTotal;
 	private TextView globalHighRanking[];
+	private TextView globalNickname[];
+	private TextView globalHighScoreNum[];
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +42,31 @@ public class HighScores extends Activity {
 		populateLocalScores();
 		populateGlobalScores();
 
+		putGlobalHighScore("KKK", 222);
+	}
+
+	private void populateGlobalScores() {
+		globalHighRanking = new TextView[NUM_GLOBAL_SCORES_TO_DISPLAY];
+		globalNickname = new TextView[NUM_GLOBAL_SCORES_TO_DISPLAY];
+		globalHighScoreNum = new TextView[NUM_GLOBAL_SCORES_TO_DISPLAY];
+		//Map <String, String> globalHighScores = getGlobalHighScores();
+		
+		for (int i = 0; i < NUM_GLOBAL_SCORES_TO_DISPLAY; i++) {
+			int resID = getResources().getIdentifier(
+					"globalHighRanking" + (i + 1), "id", getPackageName());
+			globalHighRanking[i] = (TextView) findViewById(resID);
+			globalHighRanking[i].setText("" + (i + 1) + ".");
+			
+			resID = getResources().getIdentifier(
+					"globalNickname" + (i + 1), "id", getPackageName());
+			globalNickname[i] = (TextView) findViewById(resID);
+			globalNickname[i].setText("AAA");
+			
+			resID = getResources().getIdentifier(
+					"globalHighScoreNum" + (i + 1), "id", getPackageName());
+			globalHighScoreNum[i] = (TextView) findViewById(resID);
+			globalHighScoreNum[i].setText("0");
+		}
 	}
 
 	private void populateLocalScores() {
@@ -47,15 +91,75 @@ public class HighScores extends Activity {
 
 		localTotalTextView.setText("" + localTotal);
 	}
-	
-	private void populateGlobalScores() {
-		globalHighRanking = new TextView[NUM_GLOBAL_SCORES_TO_DISPLAY];
-		
-		for(int i = 0; i < NUM_GLOBAL_SCORES_TO_DISPLAY; i++) {
-			int resID = getResources().getIdentifier("globalHighRanking" + (i+1),
-				    "id", getPackageName());
-			globalHighRanking[i] = (TextView) findViewById(resID);
-			globalHighRanking[i].setText("" + (i+1) + ".");
+
+	public void putGlobalHighScore(String name, int score) {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(CrunchConstants.backendURL + "/?name="
+				+ name + "&score=" + score);
+
+		try {
+			httpclient.execute(httpget);
+
+		} catch (Exception e) {
+			Log.v("error",
+					"Exception thrown! Check out calls to 'getGlobalHighScores().'");
 		}
 	}
+
+	public static Map<String,String> getGlobalHighScores() {
+
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet globalScores = new HttpGet(CrunchConstants.backendURL
+				+ "/?hs=1");
+		Map<String, String> globalHighScores = new HashMap<String, String>();
+
+		HttpResponse response;
+
+		try {
+			response = httpclient.execute(globalScores);
+
+			Log.i("getGlobalHighScores", response.getStatusLine().toString());
+			HttpEntity entity = response.getEntity();
+
+			if (entity != null) {
+
+				InputStream instream = entity.getContent();
+				String result = convertStreamToString(instream);
+				globalHighScores = new Gson().fromJson(result,
+						new TypeToken<Map<String, String>>() {
+						}.getType());
+				instream.close();
+			}
+
+		} catch (Exception e) {
+			Log.v("error",
+					"Exception thrown! Check out calls to 'getGlobalHighScores().'");
+		}
+
+		return globalHighScores;
+
+	}
+
+	private static String convertStreamToString(InputStream is) {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
+
+		String line = null;
+		try {
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
+	}
+
 }
