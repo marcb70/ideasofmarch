2fee6f612bc256100965c9771624d0ea10bd3be2
Poema Cavalcanti <poema.campos@gmail.com>
2013-03-16 14:14:16 -0700

    Timers on all game modes

--- main/src/edu/csumb/ideasofmarch/codecruncher/HexToBinary.java
+++ main/src/edu/csumb/ideasofmarch/codecruncher/HexToBinary.java
@@ -3,6 +3,7 @@ package edu.csumb.ideasofmarch.codecruncher;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -17,11 +18,40 @@ public class HexToBinary extends Activity {
 	private int solution;
 	private String [] hexValues = new String[16];
 	
+	private CountDownTimer gameClock;
+	private CountDownTimer moreTimer;
+	private TextView clock;
+	private int score = 0;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.hex_to_binary);
+	    
+	    gameClock = new CountDownTimer(60000,1000){
+			@Override
+			public void onFinish() {
+				// TODO Auto-generated method stub
+				clock.setText("Game Over " + score + " points");
+			}
+			@Override
+			public void onTick(long millisUntilFinished) {
+				clock.setText("Decimal to Binary : " + millisUntilFinished / 1000);				
+			}    	
+	    };
+	    
+	    moreTimer = new CountDownTimer(60000, 5000){
+			@Override
+			public void onFinish() {
+				// TODO Auto-generated method stub				
+			}
+			@Override
+			public void onTick(long millisUntilFinished) {
+				// TODO Auto-generated method stub				
+			}	    	
+	    };
+	    
+	    clock = (TextView) findViewById(R.id.title);
 	    
 	    submitButton = (Button) findViewById(R.id.submitButton);
 	    
@@ -48,12 +78,14 @@ public class HexToBinary extends Activity {
 	    binarySolution.setText(Integer.toBinaryString(solution));
 	    
 	    submitButton.setOnClickListener(new SubmitButtonListener());
+	    gameClock.start();
 	}
 
     private class SubmitButtonListener implements OnClickListener {
 		public void onClick(View view) {
 			
 			if(hexInput.getValue() == solution) {
+				increaseScore(solution);
 				binarySolution.setTextColor(Color.GREEN);
 				solution = newSolution();
 				binarySolution.setText(Integer.toBinaryString(solution));
@@ -70,5 +102,9 @@ public class HexToBinary extends Activity {
     
     public int newSolution() {
     	return (int) Math.floor(Math.random()*16);
+    }
+    
+    public void increaseScore(int points){
+    	score += points;
     }
 }
