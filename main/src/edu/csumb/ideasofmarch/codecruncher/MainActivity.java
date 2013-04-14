faa1380878a4fde4911ae96bf34bd89603e288c9
Jeremy Colwick <jlcolwick@gmail.com>
2013-04-13 18:32:35 -0700

    Fixed where name is no longer automatically asked for. Still need to allow name change, and save scores by name to allow multiple players per device.

--- main/src/edu/csumb/ideasofmarch/codecruncher/MainActivity.java
+++ main/src/edu/csumb/ideasofmarch/codecruncher/MainActivity.java
@@ -18,10 +18,12 @@ import android.os.StrictMode;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnKeyListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.text.Editable;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,29 +37,28 @@ public class MainActivity extends Activity {
 	Button donateButton;
 	Button highScoresButton;
 	Button quitButton;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        disabledPolicy();
-        getUserName();
-        initLocalPreferences();
-        initLocalScores();
-        
-        newGameButton = (Button) findViewById(R.id.newGameButton);
-        donateButton = (Button) findViewById(R.id.donateButton);
-        highScoresButton = (Button) findViewById(R.id.highScoresButton);
-        quitButton = (Button) findViewById(R.id.quitButton);
-        
-        newGameButton.setOnClickListener(new NewGameButtonListener());
-        donateButton.setOnClickListener(new DonateButtonListener());
-        highScoresButton.setOnClickListener(new HighScoresButtonListener());
-        quitButton.setOnClickListener(new QuitButtonListener());
-    }
 
-    private void getUserName() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		disabledPolicy();
+		initLocalPreferences();
+		initLocalScores();
+
+		newGameButton = (Button) findViewById(R.id.newGameButton);
+		donateButton = (Button) findViewById(R.id.donateButton);
+		highScoresButton = (Button) findViewById(R.id.highScoresButton);
+		quitButton = (Button) findViewById(R.id.quitButton);
+
+		newGameButton.setOnClickListener(new NewGameButtonListener());
+		donateButton.setOnClickListener(new DonateButtonListener());
+		highScoresButton.setOnClickListener(new HighScoresButtonListener());
+		quitButton.setOnClickListener(new QuitButtonListener());
+	}
+
+	private void getUserName() {
 
 		AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
@@ -73,7 +74,22 @@ public class MainActivity extends Activity {
 				Editable value = input.getText();
 				// Do something with value!
 				updateUserName(value.toString());
-				
+
+			}
+		});
+
+		input.setOnKeyListener(new View.OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// TODO Auto-generated method stub
+				if (keyCode == KeyEvent.KEYCODE_ENTER) {
+					Editable value = input.getText();
+					// Do something with value!
+					updateUserName(value.toString());
+					return true;
+				}
+				return false;
+
 			}
 		});
 
@@ -81,23 +97,25 @@ public class MainActivity extends Activity {
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int whichButton) {
 						// clicked cancel, set name to UNK.
-						updateUserName("UNK");
+						updateUserName("Anonymous");
 					}
 				});
 
 		alert.show();
 
 	}
-    
-    private void updateUserName(String name){
-    	
-    	CrunchConstants.myPreferencesMap.remove(CrunchConstants.JSON_NAME);
-    	CrunchConstants.myPreferencesMap.put(CrunchConstants.JSON_NAME, name);
-    	
-    	try {
-			FileOutputStream fos = openFileOutput(CrunchConstants.PREFERENCES_FILENAME, MODE_PRIVATE);
-			
-			fos.write(new Gson().toJson(CrunchConstants.myPreferencesMap).getBytes());
+
+	private void updateUserName(String name) {
+
+		CrunchConstants.myPreferencesMap.remove(CrunchConstants.JSON_NAME);
+		CrunchConstants.myPreferencesMap.put(CrunchConstants.JSON_NAME, name);
+
+		try {
+			FileOutputStream fos = openFileOutput(
+					CrunchConstants.PREFERENCES_FILENAME, MODE_PRIVATE);
+
+			fos.write(new Gson().toJson(CrunchConstants.myPreferencesMap)
+					.getBytes());
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -106,98 +124,95 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-     
-        return true;
-    }
-    
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-        case R.id.action_settings:
-            startSettings();
-            return true;
-        case R.id.action_help:
-            startHelp();
-            return true;
-        case R.id.action_about:
-        	startAbout();
-        	return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
-    
-    public void startSettings() {
-    	startActivity(new Intent(this, Settings.class));
-    }
-    
-    public void startHelp() {
-    	startActivity(new Intent(this, Help.class));
-    }
-    
-    public void startAbout() {
-    	startActivity(new Intent(this, About.class));
-    }
-    
-    private class NewGameButtonListener implements OnClickListener {
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			startSettings();
+			return true;
+		case R.id.action_help:
+			startHelp();
+			return true;
+		case R.id.action_about:
+			startAbout();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void startSettings() {
+		startActivity(new Intent(this, Settings.class));
+	}
+
+	public void startHelp() {
+		startActivity(new Intent(this, Help.class));
+	}
+
+	public void startAbout() {
+		startActivity(new Intent(this, About.class));
+	}
+
+	private class NewGameButtonListener implements OnClickListener {
 		public void onClick(View view) {
 			startChooseMode();
 		}
-    }
-    
-    public void startChooseMode() {
-    	startActivity(new Intent(this, ChooseMode.class));
-    }
-    
-    private class DonateButtonListener implements OnClickListener {
+	}
+
+	public void startChooseMode() {
+		startActivity(new Intent(this, ChooseMode.class));
+	}
+
+	private class DonateButtonListener implements OnClickListener {
 		public void onClick(View view) {
 			startDonate();
 		}
-    }
+	}
 
-    private void startDonate() {
-    	goToUrl ("https://milcyber.org/#donate");
-    }
-    
-    private void goToUrl (String url) {
-        Uri uriUrl = Uri.parse(url);
-        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-        startActivity(launchBrowser);
-    }
-    
-    private class HighScoresButtonListener implements OnClickListener {
+	private void startDonate() {
+		goToUrl("https://milcyber.org/#donate");
+	}
+
+	private void goToUrl(String url) {
+		Uri uriUrl = Uri.parse(url);
+		Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+		startActivity(launchBrowser);
+	}
+
+	private class HighScoresButtonListener implements OnClickListener {
 		public void onClick(View view) {
 			startHighScores();
 		}
-    }
-    
-    public void startHighScores() {
-    	startActivity(new Intent(this, HighScores.class));
-    }
-    
-    private class QuitButtonListener implements OnClickListener {
+	}
+
+	public void startHighScores() {
+		startActivity(new Intent(this, HighScores.class));
+	}
+
+	private class QuitButtonListener implements OnClickListener {
 		public void onClick(View view) {
 			System.exit(0);
 		}
-    }
-    
-    private void disabledPolicy() {
-    	if (android.os.Build.VERSION.SDK_INT > 9) {
-    		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-    		StrictMode.setThreadPolicy(policy);
-    	}
-    }
-    
+	}
 
-	
-	
+	private void disabledPolicy() {
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+		}
+	}
 
 	/**
 	 * Eats file saved local information
@@ -214,7 +229,7 @@ public class MainActivity extends Activity {
 			CrunchConstants.myScoresMap = gson.fromJson(reader,
 					new TypeToken<Map<Integer, Integer>>() {
 					}.getType());
-			
+
 			is.close();
 
 		} catch (Exception e) {
@@ -236,13 +251,14 @@ public class MainActivity extends Activity {
 			myTemporaryMap.put(CrunchConstants.HEX_TO_DECIMAL_HARD, 0);
 			myTemporaryMap.put(CrunchConstants.BINARY_TO_DECIMAL_HARD, 0);
 			myTemporaryMap.put(CrunchConstants.BINARY_TO_HEX_HARD, 0);
-			
+
 			CrunchConstants.myScoresMap = myTemporaryMap;
 		}
-		
+
 		try {
-			FileOutputStream fos = openFileOutput(CrunchConstants.SCORES_FILENAME, MODE_PRIVATE);
-			
+			FileOutputStream fos = openFileOutput(
+					CrunchConstants.SCORES_FILENAME, MODE_PRIVATE);
+
 			fos.write(new Gson().toJson(CrunchConstants.myScoresMap).getBytes());
 			fos.close();
 		} catch (FileNotFoundException e) {
@@ -252,13 +268,15 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	
 
-		
 	}
-	
-	public void initLocalPreferences(){
+
+	public void initLocalPreferences() {
+		if (CrunchConstants.myPreferencesMap != null && 
+				CrunchConstants.myPreferencesMap.containsKey(CrunchConstants.JSON_NAME) && 
+				CrunchConstants.myPreferencesMap.get(CrunchConstants.JSON_NAME).isEmpty())
+			getUserName();
+
 		try {
 
 			InputStream is = openFileInput(CrunchConstants.PREFERENCES_FILENAME);
@@ -278,21 +296,20 @@ public class MainActivity extends Activity {
 
 			Map<String, String> myTemporaryMap = new HashMap<String, String>();
 
-			//Update this to ask user for input to add to file.
+			// Update this to ask user for input to add to file.
 			myTemporaryMap.put(CrunchConstants.JSON_NAME, "XXX");
-			
+
 			CrunchConstants.myPreferencesMap = myTemporaryMap;
 		}
 
+		// Successfully init
 
-		
-		//Successfully init 
-		
-		
 		try {
-			FileOutputStream fos = openFileOutput(CrunchConstants.PREFERENCES_FILENAME, MODE_PRIVATE);
-			
-			fos.write(new Gson().toJson(CrunchConstants.myPreferencesMap).getBytes());
+			FileOutputStream fos = openFileOutput(
+					CrunchConstants.PREFERENCES_FILENAME, MODE_PRIVATE);
+
+			fos.write(new Gson().toJson(CrunchConstants.myPreferencesMap)
+					.getBytes());
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -301,7 +318,7 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 	}
-	
+
 }
