d20a79d10a840d71dc3f0bd7742e00955d62c06d
Marcus Bell <marbell@csumb.edu>
2013-04-13 18:52:23 -0700

    fixed hex modes

---
+++ main/src/edu/csumb/ideasofmarch/codecruncher/BinaryRowTest.java
@@ -0,0 +1,7 @@
+package edu.csumb.ideasofmarch.codecruncher;
+
+import junit.framework.TestCase;
+
+public class BinaryRowTest extends TestCase {
+
+}
---
+++ main/src/edu/csumb/ideasofmarch/codecruncher/HexRow.java
@@ -0,0 +1,142 @@
+package edu.csumb.ideasofmarch.codecruncher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class HexRow implements GameRow {
+	private HexRow instance = null; 
+	static int rowCount = 0;
+	private Activity ctx;
+	private LinearLayout pll,ll;
+	private int randNum, numDigits;
+	private TextView question;
+	private ToggleButton t1,t2,t3,t4;
+	private List<ToggleButton> buttonList = new ArrayList<ToggleButton>();
+	
+	public HexRow(LinearLayout aLayout, Activity aContext, int Digits) {
+		instance = this;
+		ctx = aContext;
+		pll = aLayout;
+		numDigits = Digits;
+		randNum = (int) Math.floor(Math.random() * (Math.pow(2,numDigits)));	
+		question = new Button(aContext);
+		
+		int resID = (int) Math.floor(Math.random()*3);
+		if(resID == 0) {
+			resID = R.drawable.redbutton;
+		} else if(resID == 1) {
+			resID = R.drawable.yellowbutton;
+		}else {
+			resID = R.drawable.bluebutton;
+		}
+		
+		for (int i = 0; i < numDigits; i++) {
+			t1 = new ToggleButton(aContext);
+			t1.setBackgroundResource(resID);
+			t1.setText(R.string.binaryInput); 
+			t1.setTextOff("0");
+			t1.setTextOn("1");
+			t1.setTextColor(Color.WHITE);
+			t1.setChecked(false);
+			buttonList.add(t1);
+		}
+		
+		ll = new LinearLayout(aContext);
+		for (int i = 0; i < numDigits; i++) {
+			ll.addView(buttonList.get(i));
+		}
+		question.setText("" + Integer.toHexString(randNum));
+		question.setTextColor(Color.WHITE);
+		question.setTextSize(20);
+		question.setBackgroundResource(R.drawable.graybutton);
+		question.setOnClickListener(new SubmitButtonListener());
+		ll.addView(question);
+	}
+
+	@Override
+	public boolean putNewRow() {
+		if(rowCount < 8){
+			rowCount++;
+			pll.addView(ll);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public boolean checkProblem() {
+		String answer = getGuess();
+		int intAnswer = Integer.parseInt(answer, 2);
+		
+		if(intAnswer == randNum){
+			// Sound will play in the BinaryToDecimal and BinaryToHex activity files
+			removeRow();
+			return true;
+		}else{
+			question.setTextColor(Color.RED);
+			return false;
+		}
+	}
+	
+    public String getGuess() {
+    	String binaryInput = "";
+    	
+    	for (int i = 0; i < numDigits; i++){
+			if (buttonList.get(i).isChecked()) {
+				binaryInput += "1";
+			} else {
+				binaryInput += "0";
+			}
+    	}
+    	    	
+    	return binaryInput;
+    }
+
+	@Override
+	public boolean removeRow() {
+		if(rowCount > 0){
+			ll.setVisibility(View.GONE);
+			((ViewGroup) ll.getParent()).removeView(ll);
+			rowCount--;
+			return true;
+		}else{
+			randNum = (int) Math.floor(Math.random()*(Math.pow(2,numDigits)));
+			question.setText("" + Integer.toHexString(randNum));
+		return false;
+		}
+	}
+	
+	public int score(){
+		return randNum;
+	}
+	
+	public void resetRowCount(){
+		rowCount = 0;
+	}
+	
+	private class SubmitButtonListener implements OnClickListener {
+		
+		public void onClick(View view) {
+			if(checkProblem()){
+				 if(numDigits == 4){
+					((BinaryToHex) ctx).correctAnswer(instance);
+				 }else{
+					 ((BinaryToHexHard) ctx).correctAnswer(instance);
+				 }
+			}
+		}	
+	}
+}
